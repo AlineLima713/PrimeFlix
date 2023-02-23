169fb1dd62ad8678ed9c4cb9267c2b3a839e25e9
Aline Lima <alinelima713@gmail.com>
2023-02-22 21:33:57 -0300

    add header

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+import Header from './Components/Header';
+
 
 function RoutesApp() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/filme/:id' element={<Filme />} />

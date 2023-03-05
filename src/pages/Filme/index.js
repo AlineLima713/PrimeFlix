782b282e2f33ef843656cc6d5031de64246291bd
Aline Lima <alinelima713@gmail.com>
2023-03-05 17:23:08 -0300

    movie info page created

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,65 @@
 import React from "react"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './filme-info.css';
+import api from '../../services/api';
 
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "28fc232cc001c31e8a031f419d0a14ca",
+          language: "pt-BR",
+        }
+      })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch((wrr) => {
+          console.log("filme não encontrado")
+        })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("DESMONTADO")
+    }
+
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>filmes</h1>
-    </div>
+    <div className="filme-info" >
+      <h1>{filme?.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme?.backdrop_path}`} alt={filme?.title} />
+
+      <h3>Sinopse</h3>
+      <span>{filme?.overview}</span>
+      <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div >
   )
 }
 

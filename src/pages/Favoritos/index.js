ad3cad6315e8c8edba688f4cdc7959fad6c10b06
Aline Lima <alinelima713@gmail.com>
2023-03-11 08:21:24 -0300

    added delete movie action

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -12,10 +12,21 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || [])
   }, [])
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return (item.id !== id)
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+  }
+
 
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+    {filmes.length === 0 && <span>Você ainda não possui filmes salvos :( </span>}
 
       <ul>
         {filmes.map((item) => {
@@ -24,7 +35,7 @@ function Favoritos() {
               <spam>{item.title}</spam>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           )
